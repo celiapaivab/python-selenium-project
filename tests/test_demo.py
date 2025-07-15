46023665116370e4bae89f9bfdd34d24fdb65493
celiapaivab <celiadepaivabruno@gmail.com>
2025-07-15 11:43:11 -0300

    Login and version tests created

---
+++ tests/test_demo.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.common.by import By
+from support.common import get_element, get_element_text, type_element, click_element, is_element_visible
+
+
+@pytest.fixture(scope="module")
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.close()
+    driver.quit()
+
+def test_verify_tittle(driver):
+    driver.get("http://dbankdemo.com/bank/login")
+    assert "Digital Bank" == driver.title
+
+def test_login(driver):
+    # find username element input and type jsmith@demo.io
+    # driver.find_element(By.ID, 'username').send_keys('jsmith@demo.io')
+    type_element(driver, (By.ID, 'username'), 'jsmith@demo.io')
+
+    # find password element input and type Demo123!
+    # driver.find_element(By.ID, 'password').send_keys('Demo123!')
+    type_element(driver, (By.ID, 'password'), 'Demo123!')
+
+    # click sign in button
+    # driver.find_element(By.ID, 'submit').click()
+    click_element(driver, (By.ID, 'submit'))
+
+    assert 'home' in driver.current_url
+
+def test_verify_version(driver):
+    # FAIL
+    # click on about link button
+    # driver.find_element(By.ID, 'aboutLink').click()
+    # verify version within the popup dialog
+    # assert '2.1.8.7' in driver.find_element(By.CLASS_NAME, 'modal-body').text
+
+    # PASS
+    get_element(driver, (By.ID, 'aboutLink')).click()
+    assert '2.1.8.7' in get_element_text(driver, (By.CLASS_NAME, 'modal-body'))
